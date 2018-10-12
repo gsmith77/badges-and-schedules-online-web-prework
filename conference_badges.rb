77c94636acb8b062151ac71a4d639a2fa6c0f973
grant smith <grantsmith1256@gmail.com>
2018-10-12 04:33:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees).each_slice
-  puts assign_rooms(attendees).each_slice
+  puts batch_badge_creator(attendees).each
+  puts assign_rooms(attendees).each
 end
